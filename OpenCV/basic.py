d222c8aee55edb2d98915954418000d59e35f4a4
Mark Lloyd Bastasa <74046232+syngrion@users.noreply.github.com>
2021-03-14 21:32:37 +0800

    Add files via upload

---
+++ OpenCV/basic.py
@@ -0,0 +1,34 @@
+from cv2 import cv2 as cv
+
+img = cv.imread('Photos/City.jpg')
+cv.imshow('City', img)
+
+# Converting an image to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Blur
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge Cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=4)
+cv.imshow('Dilated', dilated)
+
+#Eroding
+eroded = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('Eroded', eroded)
+
+# Resize
+resized = cv.resize(img,(500, 500), insterposition=cv.INTER_AREA)
+cv.imshow('Resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
+
+cv.waitKey(0)
