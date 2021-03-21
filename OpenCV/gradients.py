9247e2b1ba9b57b0ba11311318c13fddf9cb85ed
Mark Lloyd Bastasa <74046232+syngrion@users.noreply.github.com>
2021-03-21 15:45:01 +0800

    Add files via upload

---
+++ OpenCV/gradients.py
@@ -0,0 +1,28 @@
+from cv2 import cv2 as cv
+import numpy as np 
+
+img = cv.imread('Photos/Rizal-Park.jpg')
+cv.imshow('Rizal Park', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# laplacian
+lap = cv.Laplacian(gray, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+cv.imshow('Laplacian', lap)
+
+# Sobwl
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow('Sobel X', sobelx)
+cv.imshow('Sobel Y', sobely)
+cv.imshow('Combined Sobel', combined_sobel)
+
+
+canny = cv.Canny(gray, 150, 175)
+cv.imshow('Canny', canny)
+cv.waitKey(0)
